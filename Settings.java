572add174d4683541e6360d5c8c321ac4800db75
David <46841392+David654@users.noreply.github.com>
2021-11-24 19:19:32 +0200

    Update Settings.java

--- Settings.java
+++ Settings.java
@@ -11,7 +11,7 @@ public class Settings
     public static int cols = height / cell_size;
     public static int ticks = 10;
 
-    public static final String title = "Maze Generator";
+    public static final String title = "Game of Life";
     public static final Color bg_dead = new Color(255, 255, 255);
     public static final Color bg_alive = new Color(0, 0, 0);
 }
